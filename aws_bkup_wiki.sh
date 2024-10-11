7deba4450b33e07966687a589e4a4a13ff37bec1
Jan Dolstra <jan@jandnet.nl>
2024-10-11 14:43:51 +0200

    Change the backup format from a tarball containing both files and a database dump to only a database dump. This simplifies the backup process and reduces the amount of data that needs to be stored on S3

--- aws_bkup_wiki.sh
+++ aws_bkup_wiki.sh
@@ -2,15 +2,13 @@
 
 # Configuration
 S3_BUCKET="my-wiki-backups"
-MEDIAWIKI_PATH="/var/www/mediawiki"
 DB_NAME="wikidb"
 DB_USER="wikiuser"
 DB_PASS="securepassword"
 BACKUP_DIR="/home/johannes/mediawiki_backup"
 LAST_BACKUP_MD5="$BACKUP_DIR/last_backup.md5"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_NAME="mediawiki_backup_$TIMESTAMP"
-BACKUP_TAR="$BACKUP_DIR/$BACKUP_NAME.tar.gz"
+BACKUP_NAME="mediawiki_backup_$TIMESTAMP.sql"
 MYSQLDUMP="$BACKUP_DIR/$BACKUP_NAME.sql"
 
 # Ensure backup directory exists
@@ -18,12 +16,10 @@ mkdir -p $BACKUP_DIR
 
 # Dump the MediaWiki database
 mysqldump -u $DB_USER -p$DB_PASS $DB_NAME > $MYSQLDUMP
-
-# Archive MediaWiki files and the database dump
-tar -czf $BACKUP_TAR -C $MEDIAWIKI_PATH . -C $BACKUP_DIR $(basename $MYSQLDUMP)
+echo "MediaWiki database dump created: $MYSQLDUMP"
 
 # Generate MD5 checksum of the new backup
-NEW_MD5=$(md5sum $BACKUP_TAR | awk '{ print $1 }')
+NEW_MD5=$(md5sum $MYSQLDUMP | awk '{ print $1 }')
 
 # Compare with the last backup MD5 checksum
 if [ -f $LAST_BACKUP_MD5 ]; then
@@ -34,18 +30,14 @@ fi
 
 if [ "$NEW_MD5" != "$OLD_MD5" ]; then
   # If the MD5 checksums differ, upload the new backup to S3
-  echo "New changes detected, uploading backup to S3..."
-  aws s3 cp $BACKUP_TAR s3://$S3_BUCKET/$BACKUP_NAME.tar.gz
+  echo "New changes detected, uploading database backup to S3..."
+  aws s3 cp $MYSQLDUMP s3://$S3_BUCKET/$BACKUP_NAME
   
   # Save the new MD5 checksum
   echo $NEW_MD5 > $LAST_BACKUP_MD5
-
-  # Remove old backup files to save space
-  rm $MYSQLDUMP
 else
   echo "No changes detected, skipping backup."
   
   # Cleanup
   rm $MYSQLDUMP
-  rm $BACKUP_TAR
 fi
