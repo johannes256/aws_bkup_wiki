bf18e7cabe435c9607eb8d0b1e46ff50a86d1035
Jan Dolstra <jan@jandnet.nl>
2024-10-11 16:25:59 +0200

    feat: Implement zip compression for database backups
    - Zip database backup for improved efficiency and storage optimization.
    - Integrate zip operations into the backup script.
    - Update S3 upload command to upload the zip file instead of the SQL dump.

--- aws_bkup_wiki.sh
+++ aws_bkup_wiki.sh
@@ -9,7 +9,9 @@ BACKUP_DIR="/home/johannes/mediawiki_backup"
 LAST_BACKUP_MD5="$BACKUP_DIR/last_backup.md5"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 BACKUP_NAME="mediawiki_backup_$TIMESTAMP.sql"
+ZIP_NAME="mediawiki_backup_$TIMESTAMP.zip"  # Zip file name
 MYSQLDUMP="$BACKUP_DIR/$BACKUP_NAME"
+ZIP_FILE="$BACKUP_DIR/$ZIP_NAME"  # Full path to the zip file
 
 # Ensure backup directory exists
 mkdir -p $BACKUP_DIR
@@ -18,8 +20,12 @@ mkdir -p $BACKUP_DIR
 mysqldump -u $DB_USER -p$DB_PASS $DB_NAME > $MYSQLDUMP
 echo "MediaWiki database dump created: $MYSQLDUMP"
 
-# Generate MD5 checksum of the new backup
-NEW_MD5=$(md5sum $MYSQLDUMP | awk '{ print $1 }')
+# Create a zip archive containing the SQL dump
+zip -j "$ZIP_FILE" "$MYSQLDUMP"
+echo "Database backup zipped: $ZIP_FILE"
+
+# Generate MD5 checksum of the zip backup
+NEW_MD5=$(md5sum "$ZIP_FILE" | awk '{ print $1 }')
 
 # Compare with the last backup MD5 checksum
 if [ -f $LAST_BACKUP_MD5 ]; then
@@ -29,9 +35,9 @@ else
 fi
 
 if [ "$NEW_MD5" != "$OLD_MD5" ]; then
-  # If the MD5 checksums differ, upload the new backup to S3
-  echo "New changes detected, uploading database backup to S3..."
-  aws s3 cp $MYSQLDUMP s3://$S3_BUCKET/$BACKUP_NAME
+  # If the MD5 checksums differ, upload the new zip backup to S3
+  echo "New changes detected, uploading zip database backup to S3..."
+  aws s3 cp "$ZIP_FILE" s3://$S3_BUCKET/$(basename "$ZIP_FILE")
   
   # Save the new MD5 checksum
   echo $NEW_MD5 > $LAST_BACKUP_MD5
@@ -39,5 +45,8 @@ else
   echo "No changes detected, skipping backup."
   
   # Cleanup
-  rm $MYSQLDUMP
+  rm "$ZIP_FILE"
 fi
+
+# Cleanup the SQL dump
+rm "$MYSQLDUMP"
